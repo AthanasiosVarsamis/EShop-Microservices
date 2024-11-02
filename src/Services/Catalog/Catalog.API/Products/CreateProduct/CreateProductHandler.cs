7166c057b58f8aa4b68a47d3b7a2605c7b6b1b9b
thanos vasr <thanos19972010@windowslive.com>
2024-11-02 20:29:09 +0200

    First Microservice: - Catalog.Api with vertical slice And sqrs

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,31 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Model;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+     : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductCommandHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //1. create Product Entity from command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            //2. save to database (todo)
+
+            //3. return the createProductResult result
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
 
 var app = builder.Build();
 
 //Configure the Htpp requesr pipeline.
+app.MapCarter();
 
 app.Run();
