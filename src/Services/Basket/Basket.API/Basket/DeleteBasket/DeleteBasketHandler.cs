08c8d32ab3767e4c2f308f1c40fb1ceb927b0714
thanos vasr <thanos19972010@windowslive.com>
2025-02-02 12:27:32 +0200

    Add crud operations , logging, validation, custom exception , and docker support ,injection of servcies , in basket prject

---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Basket.API.Basket.DeleteBasket
+{
+    public record DeleteBasketCommand(string UserName): ICommand<DeleteBasketResult>;
+    public record DeleteBasketResult(bool IsSuccess);
+    public class DeleteBasketCommandVlidator : AbstractValidator<DeleteBasketCommand>
+    {
+        public DeleteBasketCommandVlidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("userName is Required");
+        }
+    }
+
+    internal class DeleteBasketCommandHandler(IBasketRepository repository)
+        : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+    {
+        public async Task<DeleteBasketResult> Handle(DeleteBasketCommand request, CancellationToken cancellationToken)
+        {
+            var result = await repository.DeleteBasket(request.UserName);
+
+            return new DeleteBasketResult(result);
+        }
+    }
+}
