737b7e3907e37f963a6c5b219c4da8a77ddf5101
thanos vasr <thanos19972010@windowslive.com>
2025-02-05 20:31:40 +0200

    Add Redis cache and decorator for CachedRepo

---
+++ src/Services/Basket/Basket.API/Data/CachedBaskedRepository.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data
+{
+    public class CachedBaskedRepository
+        (IBasketRepository repository, IDistributedCache cache) : IBasketRepository
+    {
+        public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            //first we check if it is already in cache else we cache it 
+            var cachedBasked = await cache.GetStringAsync(userName, cancellationToken);
+            if(!string.IsNullOrEmpty(cachedBasked))
+               return JsonSerializer.Deserialize<ShoppingCart>(cachedBasked)!;
+            
+            var basket =  await repository.GetBasket(userName, cancellationToken);
+            await cache.SetStringAsync(userName, JsonSerializer.Serialize(basket),cancellationToken);
+            return basket;
+        }
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            //first we storing the basket in the db after we store it in the cache
+            await repository.StoreBasket(basket, cancellationToken);
+
+            await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket), cancellationToken);
+
+            return basket;
+        }
+
+        public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+        {  
+            //first we delete the basket in the db after we removing it from the cache
+
+            await repository.DeleteBasket(userName, cancellationToken);
+
+            await cache.RemoveAsync(userName, cancellationToken);
+           
+            return true;
+
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -23,6 +23,15 @@ builder.Services.AddMarten(opts =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBaskedRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
