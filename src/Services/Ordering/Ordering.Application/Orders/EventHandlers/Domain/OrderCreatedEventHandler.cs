eab3cd68518723560925bfe063adee896a71c911
thanos vasr <thanos19972010@windowslive.com>
2025-09-20 13:17:50 +0300

    Add asp.net feature management develop flag

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,17 +1,20 @@
 ﻿using MassTransit;
+using Microsoft.FeatureManagement;
 using Ordering.Application.Extensions;
 
 namespace Ordering.Application.Orders.EventHandlers.Domain;
 //we need publishEndpoint in order to create Integration Event
 internal class OrderCreatedEventHandler
-    (IPublishEndpoint publishEndpoint ,ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
+    (IPublishEndpoint publishEndpoint,IFeatureManager featureManager ,ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
 {
     public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
         logger.LogInformation("Domain Event Handler:{DomainEvent}", domainEvent.GetType().Name);
 
-        var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
-
-        await publishEndpoint.Publish(orderCreatedIntegrationEvent,cancellationToken);
+        if(await featureManager.IsEnabledAsync("OrderFullfiment"))
+        {
+            var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+            await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        }
     }
 }
